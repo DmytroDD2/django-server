2c306c4900c2d3e2783aef0b1de4fa53f6365e6f
Dmytro <vv676888@gmail.com>
2024-02-28 14:03:14 +0200

    create111

--- mysite/urls.py
+++ mysite/urls.py
@@ -44,7 +44,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("", include("my_model.urls")),
     path("admin/", admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/schema/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 
 
