58fe1a7ea02dd52a66bd30c6b6898f26b36e118c
Dmytro <vv676888@gmail.com>
2024-02-28 13:54:59 +0200

    create19

--- mysite/urls.py
+++ mysite/urls.py
@@ -42,10 +42,9 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('api/schema/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path("admin/", admin.site.urls),
     path("my_model", include("my_model.urls")),
-    path("", RedirectView.as_view(url='/api/schema/swagger/', permanent=False)),
 
     # path("", lambda request: redirect('api/schema')),
 
